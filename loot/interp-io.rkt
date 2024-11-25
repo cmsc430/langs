f539fa5c3dbc5347d6d67c93cdeee90961a5f0d2
dvanhorn <dvanhorn@cs.umd.edu>
2024-11-25 15:35:43 +0000

    crook

--- loot/interp-io.rkt
+++ loot/interp-io.rkt
@@ -5,8 +5,12 @@
 ;; Interpret p with given string as input,
 ;; return value and collected output as string
 (define (interp/io p input)
-  (parameterize ((current-output-port (open-output-string))
-                 (current-input-port  (open-input-string input)))
-      (cons (interp p)
-            (get-output-string (current-output-port)))))
+  (define result (box #f))
+  (define output
+    (with-input-from-string input
+      (λ ()
+        (with-output-to-string
+          (λ ()
+            (set-box! result (interp p)))))))
+  (cons (unbox result) output))
 
