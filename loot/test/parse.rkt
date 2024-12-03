7c38c3dc850483ca32799b8703ab37cacf8a9cf9
dvanhorn <dvanhorn@cs.umd.edu>
2024-12-03 01:45:26 +0000

    crook

--- loot/test/parse.rkt
+++ loot/test/parse.rkt
@@ -71,5 +71,7 @@
                 (p (Match (Lit 1) (list (Var 'x)) (list (Var 'x)))))
   (check-equal? (parse '(match 1 [x y]))
                 (p (Match (Lit 1) (list (Var 'x)) (list (Var 'y)))))
+  (check-equal? (parse '(match x ['() 1]))
+                (p (Match (Var 'x) (list (Lit '())) (list (Lit 1)))))
   (check-exn exn:fail? (λ () (parse-closed '(match 1 [x y])))))
 
