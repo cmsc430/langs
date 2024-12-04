74aa7392b12567d2a197bf13a21c6f3d8353d40f
dvanhorn <dvanhorn@cs.umd.edu>
2024-12-04 02:51:01 +0000

    crook

--- hoax/heap.rkt
+++ hoax/heap.rkt
@@ -1,10 +1,16 @@
 #lang racket
 (provide alloc-box alloc-cons heap-ref heap-set
          (struct-out box-ptr)
-         (struct-out cons-ptr))
+         (struct-out cons-ptr)
+         heap-ref/n
+         alloc-vect alloc-str
+         (struct-out vect-ptr)
+         (struct-out str-ptr))
 
 (struct box-ptr (i))
 (struct cons-ptr (i))
+(struct vect-ptr (i))
+(struct str-ptr (i))
 
 ;; Value* Heap -> Answer*
 (define (alloc-box v h)
@@ -15,6 +21,16 @@
 (define (alloc-cons v1 v2 h)
   (cons (cons v2 (cons v1 h))
         (cons-ptr (length h))))
+
+;; [Listof Value*] Heap -> Answer*
+(define (alloc-vect vs h)
+  (cons (append (reverse (cons (length vs) vs)) h)
+        (vect-ptr (length h))))
+
+;; [Listof Char] Heap -> Answer*
+(define (alloc-str cs h)
+  (cons (append (reverse (cons (length cs) cs)) h)
+        (str-ptr (length h))))
 
 ;; Heap Address -> Value*
 (define (heap-ref h a)
